1734d6ea0ac57913054398661f71da2a95999d43
David <ddavidr569@gmail.com>
2019-12-17 15:49:11 -0300

    Creating expenses migrations

---
+++ moneyspare-backend/src/database/migrations/20191217184050-create-expenses.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('expenses', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.TINYINT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('expenses');
+  },
+};
