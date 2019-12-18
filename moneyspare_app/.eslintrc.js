27b2609e3ab08cb6b390cd44df324936df43722f
David <ddavidr569@gmail.com>
2019-12-18 09:46:30 -0300

    Configuring editor lint

--- moneyspare_app/.eslintrc.js
+++ moneyspare_app/.eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
-    root: true,
-    extends: '@react-native-community',
-  };
-  
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
