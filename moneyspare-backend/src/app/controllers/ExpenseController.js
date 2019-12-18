97ab7cccf96ae9b4be548bc9a5bd7ca1a69101f3
David <ddavidr569@gmail.com>
2019-12-18 08:58:56 -0300

    Refactoring code and creating new routes

--- moneyspare-backend/src/app/controllers/ExpenseController.js
+++ moneyspare-backend/src/app/controllers/ExpenseController.js
@@ -2,12 +2,12 @@ const User = require('../models/User');
 const Expense = require('../models/Expenses');
 
 module.exports = {
-  async index(req, res) {
+  async findAll(req, res) {
     const expenses = await Expense.findAll();
     return res.json(expenses);
   },
 
-  async findByUser(req, res) {
+  async findByUserId(req, res) {
     // eslint-disable-next-line camelcase
     const user_id = +req.params.user_id;
     await Expense.findAll({ where: { user_id } })
@@ -19,7 +19,7 @@ module.exports = {
       });
   },
 
-  async store(req, res) {
+  async save(req, res) {
     // eslint-disable-next-line camelcase
     const user_id = +req.params.user_id;
     const { category, description, value, type, paid } = req.body;
@@ -38,5 +38,21 @@ module.exports = {
     });
 
     return res.json(expense);
+  },
+
+  async update(req, res) {
+    // eslint-disable-next-line camelcase
+    const expense_id = +req.params.expense_id;
+    const { category, description, value, type, paid } = req.body;
+    Expense.findByPk(expense_id).then(expense => {
+      expense
+        .update({ category, description, value, type, paid })
+        .then(result => {
+          return res.status(201).json(result);
+        })
+        .catch(err => {
+          return res.status(400).json(err.message);
+        });
+    });
   },
 };
---
+++ moneyspare-backend/src/routes/AuthRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const AuthController = require('../app/controllers/AuthController');
+
+const routes = express.Router();
+
+routes.post('/login', AuthController.login);
+
+module.exports = routes;
